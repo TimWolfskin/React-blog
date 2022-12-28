f586367e299c3ef160cf0e5ecbf668b24508118f
TimWolfskin <timwolfskin013@gmail.com>
2022-12-28 01:38:31 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './pages/home/Home';
 import TopBar from './topbar/TopBar';
 
 function App() {
   return (
     <>
         <TopBar />
+        <Home />
     </>
 
   );
