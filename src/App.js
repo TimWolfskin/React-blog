9733b0c1f70c7c23818c318884b47748bf894239
TimWolfskin <timwolfskin013@gmail.com>
2022-12-29 09:20:28 -0800

    added router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 import TopBar from './components/topbar/TopBar'
 import Register from './pages/register/register';
-//import Home from './pages/home/Home';
-//import Login from './pages/login/Login';
-// import Settings from './pages/settings/Settings';
-// import Single from './pages/single/Single';
-// import Write from './pages/write/Write';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Settings from './pages/settings/Settings';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
 
 function App() {
+  const currentUser = true;
+
   return (
-    <>
+    <Router>
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      {/* <Settings /> */}
-      {/* <Login /> */}
-      <Register />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">
+          {currentUser ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">
+          {currentUser ? <Settings /> : <Login />}
+        </Route>
+      </Switch>
+    </Router>
 
   );
 }
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./topBar.css"
 
 function TopBar() {
+    const user = true
     return (
         <div className='top'>
             <div className="topLeft">
@@ -12,19 +14,48 @@ function TopBar() {
             </div>
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">
+                        <Link className="link" to="/">
+                            HOME
+                        </Link>
+                    </li>
                     <li className="topListItem">ABOUT</li>
                     <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem">
+                        <Link className="link" to="/write">
+                            WRITE
+                        </Link>
+                    </li>
+                    {user && <li className="topListItem">LOGOUT</li>}
                 </ul>
             </div>
             <div className="topRight">
-                <img className='topImg' src="https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg" alt="" />
-                <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+                {user ? (
+                    <Link className="link" to="/settings">
+                        <img
+                            className="topImg"
+                            src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                            alt=""
+                        />
+                    </Link>
+                ) : (
+                    <ul className="topList">
+                        <li className="topListItem">
+                            <Link className="link" to="/login">
+                                LOGIN
+                            </Link>
+                        </li>
+                        <li className="topListItem">
+                            <Link className="link" to="/register">
+                                REGISTER
+                            </Link>
+                        </li>
+                    </ul>
+                )}
+                <i className="topSearchIcon fas fa-search"></i>
             </div>
         </div>
-    )
+    );
 }
 
 export default TopBar
